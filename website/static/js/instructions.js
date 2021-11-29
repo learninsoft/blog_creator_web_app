5e8246db01032bd3b457231db8984991fc7c1596
Shashidhar Reddy <shashidharreddy@a-tkg.co.in>
2021-11-29 15:32:55 +0530

    Shashank - 29112021

---
+++ website/static/js/instructions.js
@@ -0,0 +1,10 @@
+function validateForm(form) {
+  console.log("checkbox checked is ", form.agree.checked);
+  if (!form.agree.checked) {
+    document.getElementById("agree_chk_error").style.visibility = "visible";
+    return false;
+  } else {
+    document.getElementById("agree_chk_error").style.visibility = "hidden";
+    return true;
+  }
+}
